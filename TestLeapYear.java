dd39e1a0cd6ee226a238eded9f16a22127e039f5
Swapnil <swapnilaher137@gmail.com>
2020-05-13 15:58:19 +0530

    setting up for test and src

---
+++ TestLeapYear.java
@@ -0,0 +1,24 @@
+  
+package leapyear;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLeapYear {
+
+	private LeapYear leapYear;
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void TestYearIsLeap() {
+		
+	}
+
+	
+}
