b235bb35c427bdd7f55a419e32931af2475f90e8
Swapnil Aher <33717709+swapi17@users.noreply.github.com>
2020-05-14 11:14:22 +0530

    not a leap year when divisible by 100 not 400 @test case2

--- LeapYear.java
+++ LeapYear.java
@@ -6,11 +6,17 @@ public class LeapYear {
 		
 		if (isMultipleOf400(year))
 		   return true;
+		else if (isMultipleOf100(year))
+		   return false;
 		
 	}
 
 	private boolean isMultipleOf400(int i) {
 		return isMultipleOf(i, 400);
+	}
+	
+	private boolean isMultipleOf100(int i) {
+		return isMultipleOf(i, 100);
 	}
 	
 	private boolean isMultipleOf(int num, int base) {
