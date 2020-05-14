095bee772fc6eb92341eb27023b7afc85f6ce244
Swapnil Aher <33717709+swapi17@users.noreply.github.com>
2020-05-14 11:04:09 +0530

    first test case -year is leap year if divisible by 400

--- TestLeapYear.java
+++ TestLeapYear.java
@@ -13,12 +13,18 @@ public class TestLeapYear {
 	@Before
 	public void setUp() throws Exception {
 		
-	}
-
-	@Test
-	public void TestYearIsLeap() {
+	    leapYear = new LeapYear();
 		
 	}
 
+	@Test
+	public void when2000_thenIsLip() {
+		boolean isLeap = this.isLeap(2000);
+		assertTrue(isLeap);	
+	}
+        
+	private boolean isLeap(int year) {
+		return leapYear.isLeap(year);
+	}
 	
 }
